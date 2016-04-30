2bf5f6bce7d57bc15e14e2159905eb1cbc6eb55f
kaipengw <kaipengw@andrew.cmu.edu>
2016-04-29 22:16:06 -0400

    Change the color and theme of the design

--- app/src/main/java/com/kennan/coffeeorder/MainActivity.java
+++ app/src/main/java/com/kennan/coffeeorder/MainActivity.java
@@ -1,42 +1,64 @@
 package com.kennan.coffeeorder;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int quantity = 0;
     private double price = 0d;
-    private double toppingPrice = 0d;
+    private String summaryMessage = null;
     private boolean isWhippedCreamToppingChecked = false;
     private boolean isChocolateToppingChecked = false;
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
     }
 
+    public static Context getContext() {
+        return mContext;
+    }
     private void displayQuantity(int quantity) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(quantity + "");
     }
 
-    private void displayPrice(double price) {
+    private void generateMessage() {
+        TextView nameEditText = (TextView) findViewById(R.id.name_edit_text);
+        String name = nameEditText.getText().toString().trim();
+        if (name == null) {
+            return;
+        } else {
+            String nameOutput = getContext().getString(R.string.order_summary_name)+ name + "\n";
+            String addChocolate = getContext().getString(R.string.order_summary_chocolate) + "? " + isChocolateToppingChecked + "\n";
+            String addWhippedCream = getContext().getString(R.string.order_summary_whipped_cream) + "? " + isWhippedCreamToppingChecked + "\n";
+            String priceOutput = "Total: $" + price + "\n";
+            String thankyouOutput = getContext().getString(R.string.thank_you) + "\n";
+            summaryMessage = nameOutput + addChocolate + addWhippedCream + priceOutput + thankyouOutput;
+        }
+    }
+
+    private void displayPrice() {
         TextView orderSummary = (TextView) findViewById(R.id.order_summary_text_view);
-        String addChocolate = "Add chocolate? " + isChocolateToppingChecked + "\n";
-        String addWhippedCream = "Add whipped cream? " + isWhippedCreamToppingChecked + "\n";
-        String nameOutput = "Name: Lyla John\n";
-        String priceOutput = "Total: $" + price + "\n";
-        String thankyouOutput = "Thank you!\n";
-        orderSummary.setText(addChocolate + addWhippedCream + nameOutput + priceOutput + thankyouOutput);
+        generateMessage();
+        orderSummary.setText(summaryMessage);
+
 
     }
     public void order(View view) {
-        displayPrice(5 * quantity + (isWhippedCreamToppingChecked ? 1.5d : 0d) + (isChocolateToppingChecked ? 2.5d : 0d));
+        price = 5 * quantity + (isWhippedCreamToppingChecked ? 1.5d : 0d) + (isChocolateToppingChecked ? 2.5d : 0d);
+        displayPrice();
     }
 
     public void minusOne(View view) {
@@ -73,7 +95,17 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             default:
-                return;
+        }
+    }
+
+    public void emailTheOrder(View view) {
+        EditText nameInput = (EditText) findViewById(R.id.name_edit_text);
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + nameInput.getText().toString().trim() +".");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, summaryMessage);
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
         }
     }
 }
